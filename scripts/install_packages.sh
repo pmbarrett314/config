328c742f196616961629a52f564ea4112abe73df
Paul Barrett <pmbarrett314@gmail.com>
2016-04-21 15:39:29 -0500

    further improvements to package installation

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -8,13 +8,25 @@ source $PERSONAL_CONFIG_DIR/scripts/os_info.sh
 id -Gn $USER | grep '\bwheel\b' >> /dev/null
 CAN_SUDO=$?
 
-if [[ $(get_distro) == "Arch" ]]; then
+PACKAGE_LIST=(vim git zsh)
+DIST=get_distro
+
+if [[ $DIST == "Arch" ]]; then
 	if [[ $CAN_SUDO == false ]]; then
 		echo "You need sudo privileges to do this"
 		return 1
 	fi
-	package_list=(vim git zsh)
-	pacman -S $package_list
+	
+	pacman -S $PACKAGE_LIST
+fi
+
+elif [[ $DIST == "Ubuntu" ]]
+	if [[ $CAN_SUDO == false ]]; then
+		echo "You need sudo privileges to do this"
+		return 1
+	fi
+
+	apt-get install $PACKAGE_LIST
 fi
 
 if command -v pip >> /dev/null && [[ $CAN_SUDO == true ]]; then
