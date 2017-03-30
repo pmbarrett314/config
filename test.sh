53be4840365cba383c7229f44e967de117434afc
Paul Barrett <pmbarrett314@gmail.com>
2017-03-30 16:29:39 -0500

    accidentally left some code in test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,1 @@
 #!/bin/sh
-
-run_base64(){
-	OS=$(get_os)
-
-	if [ "$OS" = "macos" ] ; then
-		eval "$(printf "%s" "$1" | base64 -D)"
-	else	
-		eval "$(printf "%s" "$1" | base64 -d))"
-	fi
-}
-
-run_base64 $(echo "touch test" | base64)
